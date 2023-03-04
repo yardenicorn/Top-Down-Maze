f94ebfbb3d29d4d394a20bc075d49cecb75d8fa3
Yarden Admon <yardenadmon@gmail.com>
2023-03-04 13:59:20 +0200

    new scripts for players
    each script has inheritance from PlayableCharacter script

---
+++ Assets/Scripts/BlueChar.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlueChar : PlayableCharacter
+{
+    public override void Die()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void SpecialAbility()
+    {
+
+    }
+
+    public override void TakeDamage(int howMuch)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    void ApplyDamage(IDamagable damagable)
+    {
+
+    }
+}
